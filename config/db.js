2b43ea8720c125644286029a6fb18b0391f9f240
Zaidh <zozo32888@gmail.com>
2025-12-07 15:39:53 +0300

    LLLLLLLLLLL

--- config/db.js
+++ config/db.js
@@ -1,30 +1,13 @@
 const mongoose = require("mongoose");
 
-// تعريف كائن لحفظ حالة الاتصال (يتم الاحتفاظ به بين استدعاءات الدالة)
-const connection = {}; 
-
 async function connectToDb() {
-  // 1. فحص الاتصال الحالي
-  if (connection.isConnected) {
-    console.log("Using existing database connection.");
-    return; // إذا كان متصلاً بالفعل، لا تفعل شيئاً وعد
-  }
-  
-  // 2. محاولة الاتصال إذا لم يكن متصلاً
-  try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-       // يُفضل إزالة أي خيارات تخص الـ poolSize لأن Vercel تديرها
-    });
-    
-    // 3. تحديث الحالة
-    connection.isConnected = db.connections[0].readyState;
-    console.log("Connected to database successfully.");
-
-  } catch (error) {
-    console.error("Connection failed to MongoDB!", error);
-    // قد تحتاج إلى رمي الخطأ لتعالجه Express
-    // throw error; 
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("connected to database successfully");
+    } catch (error) {
+        console.log("Connection failed to MongoDB!", error);    
+    }
 }
+
 
 module.exports = connectToDb;
